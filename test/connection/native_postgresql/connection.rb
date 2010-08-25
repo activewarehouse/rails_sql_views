171421f1bcb30ecd66c613970e537db3ee6dbcf8
Anthony Eden <anthonyeden@gmail.com>
2010-08-25 08:13:28 -0400

    Moving configuration of drivers into a YAML file.

--- test/connection/native_postgresql/connection.rb
+++ test/connection/native_postgresql/connection.rb
@@ -1,26 +1,29 @@
 print "Using native PostgreSQL\n"
 
+adapter_name = 'postgresql'
+config = YAML.load_file(File.join(File.dirname(__FILE__), '/../../connection.yml'))[adapter_name]
+
 #require 'logger'
 #ActiveRecord::Base.logger = Logger.new("debug.log")
 
 ActiveRecord::Base.configurations = {
   'rails_sql_views_unittest' => {
-    :adapter  => :postgresql,
-    :username => 'rails',
-    :password => 'rails',
-    :host     => 'localhost',
-    :database => 'rails_sql_views_unittest',
-    :encoding => 'utf8',
-    :schema_file => 'schema.sql',
+    :adapter  => adapter_name,
+    :username => config['username'],
+    :password => config['password'],
+    :host     => config['host'],
+    :database => config['database'],
+    :encoding => config['encoding'],
+    :schema_file => config['schema_file'],
   }
 }
 
-ActiveRecord::Base.establish_connection 'rails_sql_views_unittest'
+ActiveRecord::Base.establish_connection config['database']
 
 puts "Resetting database"
 conn = ActiveRecord::Base.connection
 #conn.recreate_database(conn.current_database)
 conn.reconnect!
-lines = open(File.join(File.dirname(__FILE__), ActiveRecord::Base.configurations['rails_sql_views_unittest'][:schema_file])).readlines
+lines = open(File.join(File.dirname(__FILE__), ActiveRecord::Base.configurations[config['database']][:schema_file])).readlines
 lines.join.split(';').each { |line| conn.execute(line) }
 conn.reconnect!
